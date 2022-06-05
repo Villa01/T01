5708505791b1a4be9af31b7ce6b08984be8544da
Villa01 <javillatoro1@gmail.com>
2022-06-04 21:32:22 -0600

    Added a new endpoint to substract numbers in port 3000.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 const express = require('express');
 
 const app = express();
+const app2 = express();
 
 const port = 4000;
+const port2 = 3000;
+
+app2.use(express.json())
 
 app.get('/', (req, res) => {
 
@@ -12,6 +16,20 @@ app.get('/', (req, res) => {
     })
 });
 
+app2.post('/sub', (req, res) => {
+
+    const { num1, num2 } = req.body;
+
+    res.status(200).json({
+        total: num1 - num2
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server on port ${port}`);
+})
+
+
+app2.listen(port2, () => {
+    console.log(`Server on port ${port2}`);
 })
